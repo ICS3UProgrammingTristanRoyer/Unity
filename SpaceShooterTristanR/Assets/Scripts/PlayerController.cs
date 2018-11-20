6cf89d85f365ae9663bcce6c70c2f1b3e961d9cb
TristanRoyer <36854048+TristanRoyer@users.noreply.github.com>
2018-11-20 11:47:08 -0500

    ok

---
+++ SpaceShooterTristanR/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+private RigidBody;
+
+void Start()
+{
+	rb = GetComponent<Rigidbody>();
+}
+
+{
+
+
+
+public class PlayerController : MonoBehaviour
+
+  void FixedUpdate ()
+	{
+
+	float moveHorizontal = Input.GetAxis("Horizontal");
+	float moveVertical = Input.GetAxis("vertical");
+	}
+
+}
